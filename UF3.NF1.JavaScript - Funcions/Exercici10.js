ce8796e5346c0a3db5a40f5517f12c23cc2d11cc
JoanRoura <joan.roura@institutvidreres.cat>
2021-11-29 15:21:53 +0100

    Exercici10

---
+++ UF3.NF1.JavaScript - Funcions/Exercici10.js
@@ -0,0 +1,13 @@
+var cert = true;
+var fals = false;
+
+function ex10(a,b) {
+    if (a === b) {
+        return cert;
+    }
+    else {
+        return fals;
+    }      
+}
+
+console.log(ex10('20','20'));
