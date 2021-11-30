77524a8dd58878e3180802ce15cfef913d4c3093
JoanRoura <joan.roura@institutvidreres.cat>
2021-11-30 08:52:57 +0100

    Exercici3

---
+++ UF3.NF1.JavaScript - Lògica/Exercici3.js
@@ -0,0 +1,19 @@
+var x = 7;
+
+switch (true) {
+    case x <= 4.99:
+        console.log('Suspès')
+        break;
+    case x >= 5 && x <= 5.99:
+        console.log('Suficient')
+        break;
+    case x >= 6 && x <= 6.99:
+        console.log('Bé')
+        break;
+    case x >= 7 && x <= 8.99:
+        console.log('Notable')
+        break;
+    case x >= 9 && x <= 10:
+        console.log('Excel·lent')
+        break;
+}
