aec93db48aa430b11d237d8b5e5312d52996ff2c
JoanRoura <joan.roura@institutvidreres.cat>
2021-11-30 08:23:12 +0100

    Exercici1

---
+++ UF3.NF1.JavaScript - Lògica/Exercici1.js
@@ -0,0 +1,12 @@
+var a = 3;
+
+if (a % 3 == 0) {
+    console.log('El numero es multiple de 3.')
+}
+else if (a % 7 == 0) {
+    console.log('El numero es multiple de 7.')
+}
+else 
+    console.log('El numero no es multiple ni de 3 ni de 7.') 
+
+
