36d25466f0c1792c69738a881936e0a72d30ac88
JoanRoura <joan.roura@institutvidreres.cat>
2021-12-01 17:59:25 +0100

    Exercici10

---
+++ UF3.NF1.JavaScript - Lògica/Exercici10.js
@@ -0,0 +1,16 @@
+
+function ex10(linies) {
+    for (var i = 0; i <= linies; ++i) {
+        let planta = '';
+        for (var k = i; k <= linies; ++k) { 
+        }
+        for (var j = 0; j < i * 2 - 1; ++j) {
+            planta = planta + '*';   
+        }
+        console.log(planta)
+    }
+}
+
+ex10(5);
+
+  
