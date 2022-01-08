31c9802a3efc8b79e4779b484f7210a695b52c93
JoanRoura <joan.roura@institutvidreres.cat>
2022-01-08 19:11:46 +0100

    Exercici7

---
+++ UF3.NF1.JavaScript - Estrctures de dades_v2/Exercici7.js
@@ -0,0 +1,10 @@
+let array = new Map();
+
+array.set('1', 'Hola');   
+array.set(1, 'Com');    
+array.set(true, 'Anem'); 
+
+console.log(array.get('1'));
+console.log(array.get(1));
+console.log(array.get(true));
+
